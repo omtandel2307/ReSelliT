4980087c0c5c715be08b76aed923de957ddeb019
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-20 23:57:56 +0530

    Add Books Component✅

--- src/pages/SellBooks.jsx
+++ src/pages/SellBooks.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const SellBooks = () => {
+  const uploadImage = (e) => {};
+
   return (
     <div className="min-h-screen bg-deep-purple-accent-200 flex justify-center items-center">
-      <div className="absolute w-48 h-48 rounded-xl bg-deep-purple-accent-700 -bottom-6 -right-10 transform rotate-12 hidden md:block"></div>
       <div className="py-12 px-12 bg-white rounded-2xl shadow-xl z-20 m-5">
         <div>
           <h1 className="text-3xl font-bold text-center mb-4 cursor-pointer">
@@ -94,55 +95,14 @@ const SellBooks = () => {
               >
                 Upload Image:
               </label>
-
               <div className="mb-2">
-                <input type="file" name="file" id="file" className="sr-only" />
-                <label
-                  for="file"
-                  className="relative flex min-h-[100px] items-center justify-center rounded-md border border-dashed border-[#e0e0e0] p-12 text-center"
-                >
-                  <div>
-                    <span className="mb-2 block text-xl font-semibold text-[#07074D]">
-                      Drop files here
-                    </span>
-                    <span className="mb-2 block text-base font-medium text-[#6B7280]">
-                      Or
-                    </span>
-                    <span className="inline-flex rounded border border-[#e0e0e0] py-2 px-7 text-base font-medium text-[#07074D]">
-                      Browse
-                    </span>
-                  </div>
-                </label>
-              </div>
-
-              <div className="mb-5 rounded-md bg-[#F5F7FB] py-4 px-8">
-                <div className="flex items-center justify-between">
-                  <span className="truncate pr-3 text-base font-medium text-[#07074D]">
-                    banner-design.png
-                  </span>
-                  <button className="text-[#07074D]">
-                    <svg
-                      width="10"
-                      height="10"
-                      viewBox="0 0 10 10"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.279337 0.279338C0.651787 -0.0931121 1.25565 -0.0931121 1.6281 0.279338L9.72066 8.3719C10.0931 8.74435 10.0931 9.34821 9.72066 9.72066C9.34821 10.0931 8.74435 10.0931 8.3719 9.72066L0.279337 1.6281C-0.0931125 1.25565 -0.0931125 0.651788 0.279337 0.279338Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        fill-rule="evenodd"
-                        clip-rule="evenodd"
-                        d="M0.279337 9.72066C-0.0931125 9.34821 -0.0931125 8.74435 0.279337 8.3719L8.3719 0.279338C8.74435 -0.0931127 9.34821 -0.0931123 9.72066 0.279338C10.0931 0.651787 10.0931 1.25565 9.72066 1.6281L1.6281 9.72066C1.25565 10.0931 0.651787 10.0931 0.279337 9.72066Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </button>
-                </div>
+                <input
+                  type="file"
+                  name="uploadImage"
+                  accept="image/*"
+                  onChange={uploadImage}
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-1 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                />
               </div>
             </div>
           </div>
