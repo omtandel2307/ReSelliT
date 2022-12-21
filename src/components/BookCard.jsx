e152b3a57650bcb126d62a1ac7acd48b3d6c880e
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-21 20:05:48 +0530

    Misc Changes

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -16,58 +16,58 @@ const BookCard = ({
 }) => {
   return (
     <div className="py-6 cursor-pointer min-h-[200px]">
-      <div className="flex max-w-md bg-white shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] rounded-lg overflow-hidden h-25">
-        <div className="w-1/3 bg-cover">
-          <Link to={`/buybooks/${id}`}>
+      <Link to={`/buybooks/${id}`}>
+        <div className="flex max-w-md bg-white shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] rounded-lg overflow-hidden h-25">
+          <div className="w-1/3 bg-cover">
             <img src={imageURL} alt="" className="object-cover h-full w-full" />
-          </Link>
-        </div>
-        <div className="w-2/3 p-4">
-          <h1 className="text-gray-900 font-bold text-2xl">{bookName}</h1>
-          <h3 className="text-gray-400 font-semi-bold text-md">{userName}</h3>
-
-          <p className="mt-2 text-gray-600 text-sm">{description}</p>
-          <div className="flex item-center mt-2">
-            <svg
-              className="w-5 h-5 fill-current text-gray-700"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 fill-current text-gray-700"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 fill-current text-gray-700"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 fill-current text-gray-500"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
-            <svg
-              className="w-5 h-5 fill-current text-gray-500"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-            </svg>
           </div>
-          <div className="flex item-center justify-between mt-3">
-            <h1 className="text-gray-700 font-bold text-xl">₹{price}</h1>
-            {/* <button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
+          <div className="w-2/3 p-4">
+            <h1 className="text-gray-900 font-bold text-2xl">{bookName}</h1>
+            <h3 className="text-gray-400 font-semi-bold text-md">{userName}</h3>
+
+            <p className="mt-2 text-gray-600 text-sm">{description}</p>
+            <div className="flex item-center mt-2">
+              <svg
+                className="w-5 h-5 fill-current text-gray-700"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+              </svg>
+              <svg
+                className="w-5 h-5 fill-current text-gray-700"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+              </svg>
+              <svg
+                className="w-5 h-5 fill-current text-gray-700"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+              </svg>
+              <svg
+                className="w-5 h-5 fill-current text-gray-500"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+              </svg>
+              <svg
+                className="w-5 h-5 fill-current text-gray-500"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+              </svg>
+            </div>
+            <div className="flex item-center justify-between mt-3">
+              <h1 className="text-gray-700 font-bold text-xl">₹{price}</h1>
+              {/* <button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
               Add to Cart
             </button> */}
-            <MdOutlineShoppingCart className="h-5 w-5" />
+              <MdOutlineShoppingCart className="h-5 w-5" />
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
