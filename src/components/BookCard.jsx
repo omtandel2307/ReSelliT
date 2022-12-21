a5649ff71dcfc2f26421de7fdefe4fd18f225e66
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-21 18:02:22 +0530

    Book Card Updates

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { Link } from "react-router-dom";
 
-const BookCard = () => {
+const BookCard = ({
+  bookName,
+  ownerName,
+  price,
+  phoneNumber,
+  description,
+  imageURL,
+  uid,
+  userName,
+  ownerEmail,
+  id,
+}) => {
   return (
-    <div className="py-6 cursor-pointer">
-      <div className="flex max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+    <div className="py-6 cursor-pointer min-h-[200px]">
+      <div className="flex max-w-md bg-white shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] rounded-lg overflow-hidden h-25">
         <div className="w-1/3 bg-cover">
-          <img
-            src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg"
-            alt=""
-            className="object-cover h-full w-full"
-          />
+          <Link to={`/buybooks/${id}`}>
+            <img src={imageURL} alt="" className="object-cover h-full w-full" />
+          </Link>
         </div>
         <div className="w-2/3 p-4">
-          <h1 className="text-gray-900 font-bold text-2xl">Backpack</h1>
-          <h3 className="text-gray-400 font-semi-bold text-md">Backpack</h3>
+          <h1 className="text-gray-900 font-bold text-2xl">{bookName}</h1>
+          <h3 className="text-gray-400 font-semi-bold text-md">{userName}</h3>
 
-          <p className="mt-2 text-gray-600 text-sm">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit In odit
-            exercitationem fuga id nam quia
-          </p>
+          <p className="mt-2 text-gray-600 text-sm">{description}</p>
           <div className="flex item-center mt-2">
             <svg
               className="w-5 h-5 fill-current text-gray-700"
@@ -53,11 +60,11 @@ const BookCard = () => {
             </svg>
           </div>
           <div className="flex item-center justify-between mt-3">
-            <h1 className="text-gray-700 font-bold text-xl">$220</h1>
+            <h1 className="text-gray-700 font-bold text-xl">₹{price}</h1>
             {/* <button className="px-3 py-2 bg-gray-800 text-white text-xs font-bold uppercase rounded">
               Add to Cart
             </button> */}
-            <MdOutlineShoppingCart classNameName="h-5 w-5" />
+            <MdOutlineShoppingCart className="h-5 w-5" />
           </div>
         </div>
       </div>
