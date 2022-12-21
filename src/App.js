83194a35fe61fcf95353342894982fc849be5d87
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-21 17:17:15 +0530

    Books Fetched

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import { Nav } from "./components/Nav";
@@ -7,8 +7,26 @@ import LandingPage from "./pages/LandingPage";
 import SellBooks from "./pages/SellBooks";
 import AboutUs from "./pages/AboutUs";
 import Cart from "./pages/Cart";
+import { getAllBooks } from "./utils/firebaseFunctions";
+import { useStateValue } from "./context/StateProvider";
+import { actionType } from "./context/reducer";
 
 const App = () => {
+  const [{ books }, dispatch] = useStateValue();
+
+  const fetchData = async () => {
+    await getAllBooks().then((res) => {
+      dispatch({
+        type: actionType.SET_BOOKS,
+        books: res,
+      });
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Router>
       <Nav />
