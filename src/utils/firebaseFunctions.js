1e2d2c991947d8707abbbd9417ff552dce343cdc
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-21 17:00:08 +0530

    Fetch books

--- src/utils/firebaseFunctions.js
+++ src/utils/firebaseFunctions.js
@@ -13,3 +13,9 @@ export const saveItem = async (data) => {
     merge: true,
   });
 };
+
+export const getAllBooks = async () => {
+  const books = await getDocs(
+    query(collection(firestore, "books"), orderBy("id", "desc"))
+  );
+};
