4515fd1655956870ad30ddd850f8265d0dc464c1
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-21 17:17:32 +0530

    All books Page Updates

--- src/pages/BuyBooks.jsx
+++ src/pages/BuyBooks.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import BookCard from "../components/BookCard";
+import { useStateValue } from "../context/StateProvider";
+import Loader from "./Loader";
 
 const BuyBooks = () => {
-  return <div className="min-h-[90vh]">BuyBooks</div>;
+  // const [booksArr, setBooksArr] = useState([]);
+  const [{ books }, dipatch] = useStateValue();
+
+  return (
+    <div className="min-h-[90vh] flex flex-wrap gap-4 justify-center items-center">
+      {books ? (
+        books.map((book) => <BookCard key={book.id} {...book} />)
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 };
 
 export default BuyBooks;
