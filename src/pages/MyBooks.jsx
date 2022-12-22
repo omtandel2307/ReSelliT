3f384b17657f7dfafd1cfb77f6a265e4989df6d2
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-22 11:30:46 +0530

    Updated Mybooks component

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -6,15 +6,14 @@ import { useStateValue } from "../context/StateProvider";
 const MyBooks = () => {
   const [{ user }, dispatch] = useStateValue();
   const [{ books }, dipatch] = useStateValue();
-  console.log(books);
   const filteredBooks = books.filter((book) => book.uid === user.uid);
   console.log(filteredBooks);
 
   return (
     <div className="min-h-[100vh] flex flex-wrap gap-4 items-center justify-center">
-      {filteredBooks.map((book, id) => (
-        <MyBookCard key={id} {...book} />
-      ))}
+      {filteredBooks.length === 0
+        ? "Add Some Books to Sell📚"
+        : filteredBooks.map((book, id) => <MyBookCard key={id} {...book} />)}
     </div>
   );
 };
