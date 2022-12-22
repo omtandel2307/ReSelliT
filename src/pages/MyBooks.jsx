9e2ffc051d7e763e756cd62c0c30e3547c40897b
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-22 11:20:16 +0530

    feat: My Added Books Page

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import BookCard from "../components/BookCard";
+import MyBookCard from "../components/MyBookCard";
+import { useStateValue } from "../context/StateProvider";
 
 const MyBooks = () => {
-  return <div>MyBooks</div>;
+  const [{ user }, dispatch] = useStateValue();
+  const [{ books }, dipatch] = useStateValue();
+  console.log(books);
+  const filteredBooks = books.filter((book) => book.uid === user.uid);
+  console.log(filteredBooks);
+
+  return (
+    <div className="min-h-[100vh] flex flex-wrap gap-4 items-center justify-center">
+      {filteredBooks.map((book, id) => (
+        <MyBookCard key={id} {...book} />
+      ))}
+    </div>
+  );
 };
 
 export default MyBooks;
