cc320b25b48ce67b9e28d9f519e3512d5ec2643c
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-23 16:39:46 +0530

    Added context for Cart Items

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,6 +1,7 @@
 export const actionType = {
   SET_USER: "SET_USER",
   SET_BOOKS: "SET_BOOKS",
+  SET_CARTITEMS: "SET_CARTITEMS",
 };
 
 const reducer = (state, action) => {
@@ -15,6 +16,11 @@ const reducer = (state, action) => {
       return {
         ...state,
         books: action.books,
+      };
+    case actionType.SET_CARTITEMS:
+      return {
+        ...state,
+        cartItems: action.cartItems,
       };
 
     default:
