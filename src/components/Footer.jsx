036d40a6a1602c3020089d56aeaffbe9a594a7d4
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-23 17:49:26 +0530

    Footer Updated✨

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,65 @@
-import React from "react";
-
 const Footer = () => {
   return (
-    <div className="bg-gray-900">
-      <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="grid row-gap-10 mb-8 lg:grid-cols-6">
+    <div className="relative mt-16 bg-gray-900">
+      <svg
+        className="absolute top-0 w-full h-6 -mt-5 sm:-mt-10 sm:h-16 text-gray-900"
+        preserveAspectRatio="none"
+        viewBox="0 0 1440 54"
+      >
+        <path
+          fill="currentColor"
+          d="M0 22L120 16.7C240 11 480 1.00001 720 0.700012C960 1.00001 1200 11 1320 16.7L1440 22V54H1320C1200 54 960 54 720 54C480 54 240 54 120 54H0V22Z"
+        />
+      </svg>
+      <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+          <div className="md:max-w-md lg:col-span-2">
+            <a
+              href="/"
+              aria-label="Go home"
+              title="ReSellit"
+              className="inline-flex items-center"
+            >
+              <svg
+                className="w-8 text-teal-accent-400"
+                viewBox="0 0 24 24"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                stroke="currentColor"
+                fill="none"
+              >
+                <rect x="3" y="1" width="7" height="12" />
+                <rect x="3" y="17" width="7" height="6" />
+                <rect x="14" y="1" width="7" height="6" />
+                <rect x="14" y="11" width="7" height="12" />
+              </svg>
+              <span className="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                ReSellit
+              </span>
+            </a>
+            <div className="mt-4 lg:max-w-sm">
+              <p className="text-sm text-deep-purple-50">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam.
+              </p>
+              <p className="mt-4 text-sm text-deep-purple-50">
+                Eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                beatae vitae dicta sunt explicabo.
+              </p>
+            </div>
+          </div>
           <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
             <div>
-              <p className="font-medium tracking-wide text-gray-300">
+              <p className="font-semibold tracking-wide text-teal-accent-400">
                 Category
               </p>
               <ul className="mt-2 space-y-2">
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     News
                   </a>
@@ -22,7 +67,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     World
                   </a>
@@ -30,7 +75,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Games
                   </a>
@@ -38,7 +83,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     References
                   </a>
@@ -46,12 +91,14 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <p className="font-medium tracking-wide text-gray-300">Apples</p>
+              <p className="font-semibold tracking-wide text-teal-accent-400">
+                Cherry
+              </p>
               <ul className="mt-2 space-y-2">
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Web
                   </a>
@@ -59,7 +106,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     eCommerce
                   </a>
@@ -67,7 +114,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Business
                   </a>
@@ -75,7 +122,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Entertainment
                   </a>
@@ -83,7 +130,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Portfolio
                   </a>
@@ -91,12 +138,14 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <p className="font-medium tracking-wide text-gray-300">Cherry</p>
+              <p className="font-semibold tracking-wide text-teal-accent-400">
+                Apples
+              </p>
               <ul className="mt-2 space-y-2">
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Media
                   </a>
@@ -104,7 +153,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Brochure
                   </a>
@@ -112,7 +161,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Nonprofit
                   </a>
@@ -120,7 +169,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Educational
                   </a>
@@ -128,7 +177,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Projects
                   </a>
@@ -136,14 +185,14 @@ const Footer = () => {
               </ul>
             </div>
             <div>
-              <p className="font-medium tracking-wide text-gray-300">
+              <p className="font-semibold tracking-wide text-teal-accent-400">
                 Business
               </p>
               <ul className="mt-2 space-y-2">
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Infopreneur
                   </a>
@@ -151,7 +200,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Personal
                   </a>
@@ -159,7 +208,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Wiki
                   </a>
@@ -167,7 +216,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="/"
-                    className="text-gray-500 transition-colors duration-300 hover:text-deep-purple-accent-200"
+                    className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
                   >
                     Forum
                   </a>
@@ -175,38 +224,15 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div className="md:max-w-md lg:col-span-2">
-            <span className="text-base font-medium tracking-wide text-gray-300">
-              Subscribe for updates
-            </span>
-            <form className="flex flex-col mt-4 md:flex-row">
-              <input
-                placeholder="Email"
-                required
-                type="text"
-                className="flex-grow w-full h-12 px-4 mb-3 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
-              />
-              <button
-                type="submit"
-                className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-              >
-                Subscribe
-              </button>
-            </form>
-            <p className="mt-4 text-sm text-gray-500">
-              Bacon ipsum dolor amet short ribs pig sausage prosciuto chicken
-              spare ribs salami.
-            </p>
-          </div>
         </div>
-        <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
-          <p className="text-sm text-gray-500">
+        <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+          <p className="text-sm text-gray-100">
             © Copyright 2020 Lorem Inc. All rights reserved.
           </p>
           <div className="flex items-center mt-4 space-x-4 sm:mt-0">
             <a
               href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
@@ -214,7 +240,7 @@ const Footer = () => {
             </a>
             <a
               href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
             >
               <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
                 <circle cx="15" cy="15" r="4" />
@@ -223,7 +249,7 @@ const Footer = () => {
             </a>
             <a
               href="/"
-              className="text-gray-500 transition-colors duration-300 hover:text-teal-accent-400"
+              className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
             >
               <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
                 <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
