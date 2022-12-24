0e5c68fc10b2a7a4408e9f50bbbe3fa3e7b91235
Meet Mulik <89148021+MeetMulik@users.noreply.github.com>
2022-12-24 14:25:15 +0530

    Landing Page✨

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
       <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
         <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
           <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-            Brand new
+            Book Store
           </p>
           <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
             Find the needed
